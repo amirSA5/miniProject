f3c70a40e08adc2b711dc4a60d4eebf78a4d30c1
amirSA5 <amir434soussi@gmail.com>
2024-02-05 20:24:21 +0100

    build

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/auth/LoginView.vue'
 import SignUpView from '../views/auth/SignUpView.vue'
 import HomeView from '../views/HomeView.vue'
-import ToDosViewVue from '@/views/ToDosView.vue'
+import ToDosView from '../views/ToDosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,7 +34,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: ToDosViewVue
+      component: ToDosView
     }
   ]
 })
